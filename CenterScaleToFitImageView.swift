f873c3b4f74a1099d02acf86c464b7f56ed730b1
Danish Khan <32009304+danishpathan007@users.noreply.github.com>
2021-10-07 12:47:11 +0530

    Add files via upload

---
+++ CenterScaleToFitImageView.swift
@@ -0,0 +1,35 @@
+//
+//  CenterScaleToFitImageView.swift
+//  Dazo
+//
+//  Created by Danish on 03/10/21.
+//
+
+import Foundation
+import UIKit
+
+class CenterScaleToFitImageView: UIImageView {
+    override var bounds: CGRect {
+        didSet {
+            adjustContentMode()
+        }
+    }
+
+    override var image: UIImage? {
+        didSet {
+            adjustContentMode()
+        }
+    }
+
+    func adjustContentMode() {
+        guard let image = image else {
+            return
+        }
+        if image.size.width > bounds.size.width ||
+            image.size.height > bounds.size.height {
+            contentMode = .scaleAspectFit
+        } else {
+            contentMode = .center
+        }
+    }
+}
---
+++ ClosureButton.swift
@@ -0,0 +1,50 @@
+//
+//  ClosureButton.swift
+//
+//  Created by Danish Khan on 14/12/20.
+//
+
+import Foundation
+import UIKit
+
+protocol ClosureButtonTargetViewDelegate:class {
+    func closureButtonTargetView() -> UIView!
+}
+
+enum SelectedButton: Int {
+    case assigned = 0
+    case practice = 1
+    case report = 2
+}
+
+
+class ClosureButton: UIButton {
+    var row:Int?
+    var section:Int?
+    var type: SelectedButton?
+    
+    public typealias ButtonAction = (ClosureButton)->Void
+    private var closureCallback:ButtonAction!
+    
+    var targetViewDelegate:ClosureButtonTargetViewDelegate!
+    
+    func setAction(controlEvents:UIControl.Event = UIControl.Event.touchUpInside,action:@escaping ButtonAction){
+        self.closureCallback = action
+        self.removeTarget(self, action: #selector(ClosureButton.actionPerformed), for: controlEvents)
+        self.addTarget(self, action: #selector(ClosureButton.actionPerformed), for: controlEvents)
+    }
+   
+    @objc private func actionPerformed() {
+        self.closureCallback(self)
+    }
+    
+    var targetView:UIView {
+        get{
+            if let view = targetViewDelegate.closureButtonTargetView() {
+                return view
+            }
+            fatalError("implement the ClosureButtonTargetViewDelegate to the target view")
+        }
+    }
+    
+}
