99d25934df614db4cbed2fc253708a6c13a583b2
Danish Khan <32009304+danishpathan007@users.noreply.github.com>
2021-10-07 12:46:06 +0530

    Add files via upload

---
+++ CustomButtonDesignable.swift
@@ -0,0 +1,56 @@
+//
+//  CustomButtonDesignable.swift
+//  Dazo
+//
+//  Created by Danish on 11/09/21.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class CustomButtonDesignable: UIButton {
+    @IBInspectable var cornerRadiusValue: CGFloat = 10.0 {
+        didSet {
+            setUpView()
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+    func setUpView() {
+        self.layer.cornerRadius = self.cornerRadiusValue
+        self.clipsToBounds = true
+    }
+    
+    
+    @IBInspectable var borderWidth: CGFloat {
+           set {
+               layer.borderWidth = newValue
+           }
+           get {
+               return layer.borderWidth
+           }
+       }
+
+
+       @IBInspectable var borderColor: UIColor? {
+           set {
+               guard let uiColor = newValue else { return }
+               layer.borderColor = uiColor.cgColor
+           }
+           get {
+               guard let color = layer.borderColor else { return nil }
+               return UIColor(cgColor: color)
+           }
+       }
+    
+    
+    
+}
+
